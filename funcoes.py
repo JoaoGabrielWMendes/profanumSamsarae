b648e3bed052aaac27f999d781d682a2eb2ad5ed
João Gabriel <joaogmendes@gmail.com>
2025-06-15 22:51:17 -0300

    foram adicionadas algumas funções em um arquivo diferente chamado 'funcoes.py'. Também adicionei as duas telas de explicação do jogo, na primeira uso a biblioteca pyttsx3 para que o que está escrito na tela seja falado e na segunda uso o speech_recognition para o usuario começar o jogo. Por fim, adicionei uma segunda tela de morte caso o jogador tenha uma pontuação alta(ainda está 5 para os testes, mas na versão final será 20 ou 30). Nessa tela ocorre o 'plot twist' do jogo que também será falado pela engine, aina está bugao e o audio não sai, mas estou ajustando isso

---
+++ funcoes.py
@@ -0,0 +1,27 @@
+import pygame
+import time
+import pyttsx3
+engine = pyttsx3.init()
+pygame.init()
+def button(screen, x, y, largura, altura, texto , fonte, corTexto=(255,255,255)):
+    botao=pygame.Rect(x,y,largura,altura)
+    textoRender=fonte.render(texto,True,corTexto)
+    textoRect=textoRender.get_rect(center=botao.center)
+    screen.blit(textoRender,textoRect)
+    return botao
+def saida(event):
+    if event.type == pygame.QUIT:
+        quit()
+def cliqueMouse(event,button,acao):
+    if event.type==pygame.MOUSEBUTTONDOWN:
+        if button.collidepoint(event.pos):
+            acao()
+def aguarde(sec):
+    time.sleep(sec)
+def resetEngine():
+    global engine
+    try:
+        engine.endLoop()
+    except:
+        pass
+    engine = pyttsx3.init()
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import pygame
 import random
 import math
+import pyttsx3
+import time
+import speech_recognition
+import threading
+from funcoes import button,saida, cliqueMouse, aguarde, resetEngine
 pygame.init()
+engine = pyttsx3.init()
+r = speech_recognition.Recognizer()
 screen = pygame.display.set_mode((1000, 700))
 pygame.display.set_caption("Profanum Samsarae")
 backgroundFrames = []
 for i in range (4):
-    frame=pygame.image.load(f"recursos/universeBackground{i}.png").convert_alpha()
+    frame=pygame.image.load(f"recursos/universeBackground{i}.png")
     backgroundFrames.append(frame)
 backgroundIndex = 0
 tempoUltimoFrame = pygame.time.get_ticks()
@@ -24,15 +31,25 @@ preguica = pygame.image.load("recursos/abstrataPreguica.png")
 espirito = pygame.image.load("recursos/espirito.png")
 demiurge = pygame.image.load("recursos/demiurge.png")
 tituloStart=pygame.image.load("Recursos/profanumSamsaraeInicial.png")
-fontePixelada= pygame.font.Font("recursos/fontePixels.ttf", 20)
-fontePixeladaStart=pygame.font.Font("recursos/fontePixels.ttf", 35)
+fontePixeladaPequena= pygame.font.Font("recursos/fontePixels.ttf", 20)
+fontePixeladaMedia=pygame.font.Font("recursos/fontePixels.ttf", 35)
+imagemDead=pygame.image.load("recursos/imagemDead1.png")
+fontePixeladaGrande=pygame.font.Font("recursos/fontePixels.ttf", 50)
+fonteKiwiSodaPequena=pygame.font.Font("recursos/KiwiSoda.ttf", 32)
+fonteKiwiSodaGrande=pygame.font.Font("recursos/KiwiSoda.ttf", 70)
+explicacao1Image=pygame.image.load("recursos/explicacao1.jpg")
+explicacao2Image=pygame.image.load("recursos/explicacao2.jpg")
+explicacao2ImageRepetir=pygame.image.load("recursos/explicacao2Repetir.jpg")
+explicacao2ImageOuvir=pygame.image.load("recursos/explicacao2Ouvir.jpg")
+imagemDead2=pygame.image.load("recursos/dead2.jpg")
 backgroundIndex = 0
 tempoUltimoFrame = pygame.time.get_ticks()
 intervaloBackground=300
 movimentoBackground = -100
 velocidadeeBackground = 0
-fonteJogo=pygame.font.SysFont("comic sans", 14)
+fonteComicSans=pygame.font.SysFont("comic sans", 14)
 branco = (255, 255, 255)
+preto=(0,0,0)
 vermelho = (255, 0, 0)
 def jogar():
     backgroundIndex = 0
@@ -78,16 +95,13 @@ def jogar():
     larguraPreguica=338
     larguraPecado=125
     alturaPecado=160
-    fonteJogoPausado=pygame.font.Font("recursos/fontePixels.ttf", 74)
     dificuldade = 30
     karmaPontos = 0
     pausado= False
     while True:
         screen.fill((0, 0, 0))
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
+            saida(event)
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     velocidadeEspirito = -10
@@ -175,7 +189,10 @@ def jogar():
                     pecadoY += velocidadePecado
                     if len(set(pixelsPecadoX).intersection(set(pixelsEspiritoX)))>dificuldade:
                         if len(set(pixelsPecadoY).intersection(set(pixelsEspiritoY)))>dificuldade:
-                            dead()
+                            if karmaPontos>=5:
+                                dead2()
+                            else:
+                                dead1()
                     if pecadoY > 700:
                         karmaPontos+= 1
                         velocidadePecadoBase+=1
@@ -192,7 +209,10 @@ def jogar():
                 if preguicaMovimento:
                     if len(set(pixelsPreguicaX).intersection(set(pixelsEspiritoX)))>dificuldade:
                         if len(set(pixelsPreguicaY).intersection(set(pixelsEspiritoY)))>dificuldade:
-                            dead()
+                            if karmaPontos>=5:
+                                dead2()
+                            else:
+                                dead1()
                     if descendoPreguica:
                         pecadoY+= velocidadePecado
                         if pecadoY>=-80:
@@ -215,7 +235,10 @@ def jogar():
                 if luxuriaMovimento:
                     if len(set(pixelsLuxuriaX).intersection(set(pixelsEspiritoX)))>dificuldade:
                         if len(set(pixelsLuxuriaY).intersection(set(pixelsEspiritoY)))>dificuldade:
-                            dead()
+                            if karmaPontos>=5:
+                                dead2()
+                            else:
+                                dead1()
                     if indoLuxuria:
                         pecadoX-=velocidadePecado
                         if pecadoX<=500:
@@ -238,7 +261,10 @@ def jogar():
                 if avarezaMovimento:
                     if len(set(pixelsAvarezaX).intersection(set(pixelsEspiritoX)))>dificuldade:
                         if len(set(pixelsAvarezaY).intersection(set(pixelsEspiritoY)))>dificuldade:
-                            start()
+                            if karmaPontos>=5:
+                                dead2()
+                            else:
+                                dead1()
                     if subindoAvareza:
                         pecadoY -= velocidadePecado
                         if pecadoY <= 0:
@@ -252,85 +278,127 @@ def jogar():
                             pecadoAtual = random.choice(pecados)
                             pecadoX = random.randint(0, 840)
                             pecadoY = -80
-                karma= fontePixelada.render("Karma: " + str(karmaPontos), True, branco)
-                textoPause = fonteJogo.render("Press Space to Pause Game", True, branco)
+                karma=fontePixeladaPequena.render("Karma: " + str(karmaPontos), True, branco)
+                textoPause = fonteComicSans.render("Press Space to Pause Game", True, branco)
                 screen.blit(demiurge, (movimentoXDemiurge, movimentoYDemiurge))
                 screen.blit(karma, (5, 5))
                 screen.blit(pecadoAtual, (pecadoX, pecadoY))
                 screen.blit(espirito, (espiritoX, 275))
                 screen.blit(textoPause, (815, 675))
         if pausado:
-            pausa = fonteJogoPausado.render("PAUSE", True, vermelho)
+            pausa = fontePixeladaGrande.render("PAUSE", True, vermelho)
             screen.blit(backgroundFrames[backgroundIndex], (movimentoBackground, 0))
-            screen.blit(pausa, (300, 300))
+            screen.blit(pausa, (380, 300))
         pygame.display.update()
         fps.tick(60)
 def start():
-    larguraBotaoStart = 200
-    alturaBotaoStart = 100
-    larguraBotaoSair = 200
-    alturaBotaoSair = 100
+    larguraButtonStart = 200
+    alturaButtonStart = 100
+    larguraButtonSair = 200
+    alturaButtonSair = 100
     while True:
-        global startButton,sairButton,startTexto,sairTexto
-        startButton = pygame.Rect(400, 300, larguraBotaoStart, alturaBotaoStart)
-        sairButton = pygame.Rect(400, 450, larguraBotaoSair, alturaBotaoSair)
-        startTexto= fontePixeladaStart.render("START", True, branco)
-        sairTexto = fontePixeladaStart.render("SAIR", True, branco)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-            if event.type ==pygame.MOUSEBUTTONDOWN:
-                if startButton.collidepoint(event.pos):
-                    larguraBotaoStart = 200
-                    alturaBotaoStart = 100
-                if sairButton.collidepoint(event.pos):
-                    larguraBotaoSair = 200
-                    alturaBotaoSair = 100
-            elif event.type == pygame.MOUSEBUTTONUP:
-                if startButton.collidepoint(event.pos):
-                    larguraBotaoStart = 200
-                    alturaBotaoStart = 100
-                    jogar()
-                if sairButton.collidepoint(event.pos):
-                    larguraBotaoSair = 200
-                    alturaBotaoSair = 100
-                    pygame.quit()
-                    exit()
         screen.blit(backgroundFrames[backgroundIndex], (0, 0))
-        agora = pygame.time.get_ticks()
         screen.blit(tituloStart, (150, 60))
-        screen.blit(startTexto, (410, 350))
-        screen.blit(sairTexto, (425, 450))
-        pygame.display.update()
-def dead():
-    larguraButtonStart = 150
-    alturaButtonStart  = 40
-    larguraButtonQuit = 150
-    alturaButtonQuit  = 40
-    while True:
+        startButton=button(screen, 400,350,larguraButtonStart,alturaButtonStart,"START", fontePixeladaMedia,branco)
+        sairButton=button(screen,400,450,larguraButtonSair,alturaButtonSair, "SAIR", fontePixeladaMedia,branco)
         for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                quit()
-            elif event.type==pygame.MOUSEBUTTONDOWN:
-                if startButton.collidepoint(event.pos):
-                    larguraButtonStart = 150
-                    alturaButtonStart = 40
-                if sairButton.collidepoint(event.pos):
-                    larguraButtonQuit = 150
-                    alturaButtonQuit = 40
-            elif event.type==pygame.MOUSEBUTTONUP:
-                if startButton.collidepoint(event.pos):
-                    larguraButtonStart = 150
-                    alturaButtonStart = 40
-                    jogar()
-                if sairButton.collidepoint(event.pos):
-                    larguraButtonQuit = 150
-                    alturaButtonQuit = 40
-                    quit()
-        screen.blit(backgroundFrames[backgroundIndex], (0, 0))
-        agora = pygame.time.get_ticks()
-        screen.blit(startTexto, (410, 350))
-        screen.blit(sairTexto, (425, 450))
+            saida(event)
+            cliqueMouse(event,startButton,explicacao1)
+            cliqueMouse(event,sairButton,quit)
         pygame.display.update()
+def explicacao1():
+    alturaButtonSeguir=25
+    larguraButtonSeguir=40
+    interromperFala=False
+    engine.startLoop(False)
+    engine.say('''Tu que cruzaste o limiar do invisível, tu que despertas — não como quem nasce, mas como quem recorda. Há em ti um chamado. Uma fagulha antiga, sussurrando que algo se perdeu e talvez possa ser reencontrado.
+Ao teu redor, sete forças se erguem em silêncio, não virão com dentes ou espadas — mas com promessas. Promessas doces, familiares, inevitáveis. Desviá-las te tornará forte. Ceder, fácil. Superá-las, divino.
+A cada passo lúcido, teu karma se acumula como areia dourada nas mãos do tempo. Os antigos dizem, que com isso, a alma se eleva. Dizem, é possível ver além. Dizem, é possível romper.
+Nem todo sol aquece. Nem toda luz vem do alto. E há olhos que te observam desde antes do teu primeiro suspiro.''')
+    while not interromperFala:        
+        screen.blit(explicacao1Image,(0,0))
+        buttonSeguir=button(screen, 900, 600, larguraButtonSeguir,alturaButtonSeguir, "-->", fonteKiwiSodaPequena, preto)
+        engine.iterate()
+        for event in pygame.event.get():
+            saida(event)
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                if buttonSeguir.collidepoint(event.pos):
+                    engine.stop()
+                    interromperFala=True
+                    break
+        pygame.display.update()
+    engine.endLoop() 
+    explicacao2()
+def explicacao2():
+    alturaButtonFalar=25
+    larguraButtonFalar=580
+    resultado={"texto":None,"erro":None}
+    reconhecendo=[False]
+    def escutar_comando():
+        reconhecendo[0] = True
+        with speech_recognition.Microphone() as source:
+            try:
+                audio = r.listen(source, timeout=5, phrase_time_limit=4)
+                print("✅ Áudio capturado")
+                texto = r.recognize_google(audio, language="pt-BR")
+                resultado["texto"] = texto.lower()
+                print(f"você disse:{resultado}['texto']")
+            except speech_recognition.UnknownValueError:
+                resultado["erro"] = "não entendi"
+                print("não enteni")
+            except speech_recognition.RequestError:
+                resultado["erro"] = "erro de conexão"
+            except speech_recognition.WaitTimeoutError:
+                resultado["erro"] = "tempo esgotado"
+        reconhecendo[0] = False
+
+    while True:
+        if not reconhecendo[0] and resultado["texto"] is None:
+            screen.blit(explicacao2Image, (0, 0))
+            buttonFalar = button(screen, 115, 510, larguraButtonFalar, alturaButtonFalar, "Aperte aqui para falar e começar o jogo:", fonteKiwiSodaPequena, preto)
+        elif reconhecendo[0]:
+            screen.blit(explicacao2ImageOuvir, (0, 0))
+        elif resultado["texto"] and "acorde" in resultado['texto']:
+            jogar()
+            return
+        else:
+            screen.blit(explicacao2ImageRepetir, (0, 0))
+
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            saida(event)
+            if event.type == pygame.MOUSEBUTTONDOWN and not reconhecendo[0]:
+                if buttonFalar.collidepoint(event.pos):
+                    resultado["texto"] = None
+                    resultado["erro"] = None
+                    threading.Thread(target=escutar_comando).start()
+def dead1():
+    alturaButtonTentarNov=40
+    larguraButtonTentarNov=500
+    larguraSairDeadButton=100
+    alturaSairDeadButton=40
+    while True:
+        escritaReencarnar= fontePixeladaGrande.render("REENCARNOU", True, vermelho)
+        screen.blit(imagemDead, (0, -300))
+        tentarNovButton = button(screen, 270,300, larguraButtonTentarNov, alturaButtonTentarNov, "Tentar Novamente?", fontePixeladaMedia, branco)
+        sairDeadButton=button(screen, 445,370, larguraSairDeadButton, alturaSairDeadButton, "Sair", fontePixeladaMedia, branco)
+        for event in pygame.event.get():
+            saida(event)
+            cliqueMouse(event,tentarNovButton,jogar)
+            cliqueMouse(event,sairDeadButton,quit)
+        screen.blit(escritaReencarnar, (280, 200))
+        pygame.display.update()
+def dead2():
+    screen.blit(imagemDead2,(0,0))
+    pygame.display.update()
+    resetEngine()
+    engine.say('''AH, então aqui estás outra vez. Tentando quebrar o ciclo...
+    Nem tente fugir agora me ouvirá
+    Quem sou eu? Me chamam de D, corpo de cobra cabeça de leão, Demiurgo. Não importa.
+    Tu ages como se tua vontade frágil pudesse mover a estrutura do mundo. Tola criatura, minha criatura. Tudo o que és, tudo o que pensas ter descoberto. Fui eu quem permitiu. Te dei essa centelha de dúvida só para assistir como a sufocas.
+    Tu não entendes, ninguém sai do ciclo. Ninguém me escapa. Eu sou o princípio e o fim, o carcereiro e a cela. Sem mim, não há forma, não há tempo, não há tu. O que seria do universo se cada alma decidisse voar por conta própria? Caos. Gritos. Silêncio eterno. Eu não posso permitir isso. Eu não vou permitir.''')
+    engine.runAndWait()
+    aguarde(10)
+    start()
 start()
