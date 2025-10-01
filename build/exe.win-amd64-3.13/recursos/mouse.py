ced2135c02526b1dc03694d9e103f4d865d4cbdb
João GWM <joaogwmendes@gmail.com>
2025-10-01 08:55:36 -0300

    build excluido

--- build/exe.win-amd64-3.13/recursos/mouse.py
+++
@@ -1,8 +0,0 @@
-import pygame
-def saida(event):
-    if event.type == pygame.QUIT:
-        quit()
-def cliqueMouse(event,button,acao):
-    if event.type==pygame.MOUSEBUTTONDOWN:
-        if button.collidepoint(event.pos):
-            acao()
